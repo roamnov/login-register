e98a05a21b234eabd136759647914655f0203e5a
roamnov <45330547+roamnov@users.noreply.github.com>
2023-05-31 10:44:58 +0300

    OnOpen

--- src/SignUp/SelectFio.jsx
+++ src/SignUp/SelectFio.jsx
@@ -1,57 +1,56 @@
-import  {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   Grid,
   Autocomplete,
   TextField,
   CircularProgress,
-  Skeleton
+  Skeleton,
 } from "@mui/material";
 import axios from "axios";
-import { CssTextField } from '../SignIn';
-
-
+import { CssTextField } from "../SignIn";
 
 const SelectUser = (props) => {
- 
   const [value, setValue] = useState();
-  const [users, setUserList] = useState(new Array);
+  const [users, setUserList] = useState(new Array());
   const [loading, setLoad] = useState(true);
-  
+
   useEffect(() => {
     setUserList([]);
-  
   }, [props.inn]);
 
-  let url = window.FIO_url
+  let url = window.FIO_url;
   // let url = `${document.location.origin}/mobile~registration/values?type=snils&inn=${props.inn}`;
- 
 
   const getUser = () => {
-   
-      //setUserList([])
-      setLoad(true);
-      let params = new Map();
-      params.set('prefix','project')
-      params.set('comand', 'GetRegistryValues')
-      params.set('type', 'snils')
-      params.set('inn',props.inn);
-      
-      axios.get(url + `&inn=${props.inn}`).then((response) => {
-         
-          if (Object.keys(response.data).length == 0){
-            setUserList([]);
-          }else{
-            setLoad(false);
-            setUserList(response.data);
-          }
-        });/* */
-     
+    //setUserList([])
+    setLoad(true);
+    let params = new Map();
+    params.set("prefix", "project");
+    params.set("comand", "GetRegistryValues");
+    params.set("type", "snils");
+    params.set("inn", props.inn);
+
+    axios.get(url + `&inn=${props.inn}`).then((response) => {
+      if (Object.keys(response.data).length == 0) {
+        setUserList([]);
+      } else {
+        setLoad(false);
+        setUserList(response.data);
+      }
+    }); /* */
   };
   return (
     <Grid item xs>
       <Autocomplete
         loading={loading}
-        loadingText={props.inn== undefined?"Необходимо выбрать организацию":<CircularProgress/>}
+        loadingText={
+          props.inn == undefined ? (
+            "Необходимо выбрать организацию"
+          ) : (
+            <CircularProgress />
+          )
+        }
+        onOpen={getUser}
         disableClearable
         options={users}
         getOptionLabel={(option) => option.name}
@@ -59,10 +58,17 @@ const SelectUser = (props) => {
         fullWidth
         value={value}
         onChange={(event, newValue) => {
-          props.setBackInfo(newValue.snils)
+          props.setBackInfo(newValue.snils);
         }}
         renderInput={(params) => (
-          <CssTextField {...params} onClick={ getUser} required id="snils" label="ФИО" name="snils" error={props.error.search("snils") !== -1} />
+          <CssTextField
+            {...params}
+            required
+            id="snils"
+            label="ФИО"
+            name="snils"
+            error={props.error.search("snils") !== -1}
+          />
         )}
       />
     </Grid>
--- src/SignUp/SelectOrg.jsx
+++ src/SignUp/SelectOrg.jsx
@@ -1,61 +1,54 @@
-import  {  useState } from "react";
+import { useState } from "react";
 import {
   Grid,
   Autocomplete,
   TextField,
   CircularProgress,
-  styled
+  styled,
 } from "@mui/material";
-import { CssTextField } from '../SignIn';
+import { CssTextField } from "../SignIn";
 import axios from "axios";
 
-
-
-
-
 const SelectOrg = (props) => {
- 
   const [value, setValue] = useState();
   const [inputValue, setInputValue] = useState("");
   const [orgs, setOrgList] = useState([]);
   const [loading, setLoad] = useState(true);
 
   const url = window.ORG_url;
- 
 
   const getOrg = () => {
+    setLoad(true);
+    let params = new Map();
+    params.set("prefix", "project");
+    params.set("comand", "GetRegistryValues");
+    params.set("type", "inn");
 
-      setLoad(true);
-      let params = new Map();
-      params.set('prefix','project')
-      params.set('comand', 'GetRegistryValues')
-      params.set('type', 'inn')
-      
-      
-      axios.get(url)
-        .then((response) => {
-          if (Object.keys(response.data).length == 0){
-            setOrgList([]);
-          }else{
-            setLoad(false);
-            setOrgList(response.data);
-
-          }
-          
-        });/**/
-    
+    axios.get(url).then((response) => {
+      if (Object.keys(response.data).length == 0) {
+        setOrgList([]);
+      } else {
+        setLoad(false);
+        setOrgList(response.data);
+      }
+    }); /**/
   };
 
   return (
     <Grid item xs={12} sm={12}>
       <Autocomplete
         loading={loading}
-        loadingText={<CircularProgress/>}
+        onOpen={()=>{
+          setOrgList([]);
+          getOrg();
+        }}
+        loadingText={<CircularProgress />}
         disableClearable
         disablePortal
         fullWidth
         value={value}
         onChange={(event, newValue) => {
+          setValue(value)
           props.setBackInfo(newValue.inn);
         }}
         inputValue={inputValue}
@@ -65,7 +58,15 @@ const SelectOrg = (props) => {
         options={orgs}
         getOptionLabel={(option) => option.name}
         renderInput={(params) => (
-          <CssTextField {...params} required onClick={function(event){setOrgList([]); getOrg();}} id="inn" label="Организация" name="inn" error={props.error.search("inn") !== -1} sx={{}} />
+          <CssTextField
+            {...params}
+            required
+            id="inn"
+            label="Организация"
+            name="inn"
+            error={props.error.search("inn") !== -1}
+            sx={{}}
+          />
         )}
       />
     </Grid>
